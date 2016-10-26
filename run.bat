82acdfeaed6a271c10321b5ac9f7e231933cad0e
Kenneth V. Langga <klangga@gmail.com>
2016-10-26 15:22:52 +0800

    Added main runner script

---
+++ run.bat
@@ -0,0 +1,6 @@
+@echo off
+
+set PYTHON_EXE_PATH=C:\Python27\python.exe
+
+echo "Running multiple models in 1 VM..."
+"%PYTHON_EXE_PATH%" -u run.py
---
+++ run.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+import time
+
+# Check if run dir exists
+RUNDIR = os.path.abspath('run')
+if os.path.isdir(RUNDIR):
+    while True:
+        count = 0.
+        # List folder contents
+        for c in os.path.listdir(RUNDIR):
+            # Check if it's a batch file
+            if c.endswith('.bat'):
+                # Run batch file
+                print '#' * 40, 'Running', c, '#' * 40
+                run = subprocess.Popen(c, cwd=RUNDIR)
+                run.wait()
+                count += 1
+        # Sleep for a set interval
+        dur = 60 / count * 60
+        print '#' * 40, 'Sleep for', dur, 'secs #' * 40
+        time.sleep(dur)
