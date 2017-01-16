7e52fcda319356ba8de99271a191376e7a7b509d
Kenneth V. Langga <klangga@gmail.com>
2017-01-16 14:45:20 +0800

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+GIT='/cygdrive/c/Program Files/Git/bin/git.exe'
+INSTALL_PATH='/cygdrive/c/hec-automation'
+
 echo
 echo Fixing git config...
-'/cygdrive/c/Program Files/Git/bin/git.exe' config --global core.autocrlf input
+"$GIT" config --global core.autocrlf input
 
 echo
 echo Pulling latest code from branch...
-out=$( '/cygdrive/c/Program Files/Git/bin/git.exe' pull origin prod_v2 )
+out=$( "$GIT" pull origin prod_v2 )
 echo "$out" | grep 'Updating'
 if [ $? -eq 0 ]; then
     echo Update found...
@@ -15,9 +18,9 @@ if [ $? -eq 0 ]; then
     /cygdrive/c/Python27/Scripts/pip.exe install -r requirements.txt
 
     echo Restarting script...
-    exec 'run.sh'
+    exec "$INSTALL_PATH/run.sh"
 fi
 
 echo
 echo Running multiple models in 1 VM...
-/cygdrive/c/Python27/python.exe -u run.py
+/cygdrive/c/Python27/python.exe -u "$INSTALL_PATH/run.py"
