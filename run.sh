e1f36cddceedc480cbb839f5b66ba426113a73d5
Kenneth V. Langga <klangga@gmail.com>
2017-01-16 14:32:54 +0800

    Added run.sh

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo
+echo Pulling latest code from branch...
+out=$( '/cygdrive/c/Program Files/Git/bin/git.exe' pull origin prod_v2 )
+echo "$out" | grep 'Updating'
+if [ $? -eq 0 ]; then
+    echo Update found...
+
+    echo Reinstalling requirements...
+    /cygdrive/c/Python27/Scripts/pip.exe install -r requirements.txt
+
+    echo Restarting script...
+    exec 'run.sh'
+fi
+
+echo
+echo Running multiple models in 1 VM...
+/cygdrive/c/Python27/python.exe -u run.py
